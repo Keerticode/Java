c0bc87611cb4e7c350b9a449df96da7b376609e7
Keerticode <keertihiregoudar13@gmail.com>
2025-10-08 22:00:10 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks[0] = 98;
+        marks[1] = 92;
+        marks[2] = 95;
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }  
+}
